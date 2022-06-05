0e3e3656ba482660c3fcf3e2a6d9701ac297ed51
patlayz24 <71376957+patlayz24@users.noreply.github.com>
2022-06-05 23:54:30 +0700

    Update ApiService.kt
    ini uda gw coba api nya ya, tolong di cek nal

--- app/src/main/java/com/example/trafilt/api/ApiService.kt
+++ app/src/main/java/com/example/trafilt/api/ApiService.kt
@@ -1,5 +1,23 @@
 package com.example.trafilt.api
 
-interface ApiService {
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
 
+interface ApiService {
+    @FormUrlEncoded
+    @POST("login")
+    fun login(
+        @Field("email") email : String,
+        @Field("password") password : String
+    ): Call<Response>
+
+    @FormUrlEncoded
+    @POST("register")
+    fun register(
+        @Field("name") name : String,
+        @Field("email") email : String,
+        @Field("password") password: String
+    ): Call<BaseResponse>
 }
