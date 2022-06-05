715dad850d762abb6223d9859f2dd38cd179589c
patlayz24 <71376957+patlayz24@users.noreply.github.com>
2022-06-05 23:54:58 +0700

    Update Response.kt

--- app/src/main/java/com/example/trafilt/api/Response.kt
+++ app/src/main/java/com/example/trafilt/api/Response.kt
@@ -1,2 +1,35 @@
 package com.example.trafilt.api
 
+import com.google.gson.annotations.SerializedName
+
+data class Response(
+
+    @field:SerializedName("loginResult")
+    val loginResult: LoginResult,
+
+    @field:SerializedName("error")
+    val error: Boolean,
+
+    @field:SerializedName("message")
+    val message: String
+)
+
+data class LoginResult(
+
+    @field:SerializedName("name")
+    val name: String,
+
+    @field:SerializedName("userId")
+    val userId: String,
+
+    @field:SerializedName("token")
+    val token: String
+)
+data class BaseResponse(
+
+    @field:SerializedName("error")
+    val error: Boolean,
+
+    @field:SerializedName("message")
+    val message: String
+)
